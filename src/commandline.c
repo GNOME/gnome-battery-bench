f179b22a32ba89fddff7488d99f45b69a4ee3cb3
Owen W. Taylor <otaylor@fishsoup.net>
2015-01-02 13:53:21 -0500

    Rework command line client
    Rewrite the command parsing using GOptionContext. Use a
    
     gbb <subcommand>
    
    syntax. Rename 'test' to 'monitor'.

--- src/commandline.c
+++ src/commandline.c
@@ -20,83 +20,6 @@
 #include "xinput-wait.h"
 #include "util.h"
 
-void usage(void)
-    __attribute__ ((noreturn));
-
-void
-usage(void)
-{
-    die("Usage: gbb [--record|--playback|--remote-playback|--test]");
-}
-
-static void
-record(void)
-{
-    GbbEventRecorder *recorder;
-
-    Display *display = XOpenDisplay(NULL);
-    if (!display)
-        die("Can't open X display %s", XDisplayName(NULL));
-
-    recorder = gbb_event_recorder_new(display, NULL);
-    gbb_event_recorder_record(recorder);
-    gbb_event_recorder_free(recorder);
-}
-
-static void
-on_player_finished(GbbEventPlayer *player,
-                   GMainLoop      *loop)
-{
-    g_main_loop_quit(loop);
-}
-
-static void
-on_player_ready(GObject      *source_object,
-                GAsyncResult *result,
-                gpointer      data)
-{
-    GError *error = NULL;
-    GMainLoop *loop = data;
-
-    if (!gbb_xinput_wait_finish(GBB_EVENT_PLAYER(source_object),
-                                result,
-                                &error)) {
-        die("Could not wait for devices to appear: %s\n", error->message);
-    }
-
-    g_main_loop_quit(loop);
-}
-
-static void
-do_playback(GbbEventPlayer *player,
-            const char     *filename)
-{
-    GMainLoop *loop = g_main_loop_new (NULL, FALSE);
-
-    gbb_xinput_wait(player, NULL,
-                    NULL, on_player_ready, loop);
-    g_main_loop_run (loop);
-
-    g_signal_connect(player, "finished",
-                     G_CALLBACK(on_player_finished), loop);
-    gbb_event_player_play_file(player, filename);
-    g_main_loop_run (loop);
-}
-
-static void
-playback(const char *filename)
-{
-    GbbEventPlayer *player = GBB_EVENT_PLAYER(gbb_evdev_player_new("Gnome Battery Bench Test"));
-    do_playback(player, filename);
-}
-
-static void
-remote_playback(const char *filename)
-{
-    GbbEventPlayer *player = GBB_EVENT_PLAYER(gbb_remote_player_new("Gnome Battery Bench Test"));
-    do_playback(player, filename);
-}
-
 static GbbPowerState *start_state;
 
 static void
@@ -131,8 +54,13 @@ on_power_monitor_changed(GbbPowerMonitor *monitor)
 
 }
 
-static void
-test(void)
+static GOptionEntry monitor_options[] =
+{
+    { NULL }
+};
+
+static int
+monitor(int argc, char **argv)
 {
     GbbPowerMonitor *monitor;
     GMainLoop *loop;
@@ -144,37 +72,163 @@ test(void)
 
     loop = g_main_loop_new (NULL, FALSE);
     g_main_loop_run (loop);
+
+    return 0;
+}
+
+static void
+on_player_finished(GbbEventPlayer *player,
+                   GMainLoop      *loop)
+{
+    g_main_loop_quit(loop);
+}
+
+static void
+on_player_ready(GObject      *source_object,
+                GAsyncResult *result,
+                gpointer      data)
+{
+    GError *error = NULL;
+    GMainLoop *loop = data;
+
+    if (!gbb_xinput_wait_finish(GBB_EVENT_PLAYER(source_object),
+                                result,
+                                &error)) {
+        die("Could not wait for devices to appear: %s\n", error->message);
+    }
+
+    g_main_loop_quit(loop);
+}
+
+static int
+do_play(GbbEventPlayer *player,
+        int             argc,
+        char          **argv)
+{
+    GMainLoop *loop = g_main_loop_new (NULL, FALSE);
+
+    gbb_xinput_wait(player, NULL,
+                    NULL, on_player_ready, loop);
+    g_main_loop_run (loop);
+
+    g_signal_connect(player, "finished",
+                     G_CALLBACK(on_player_finished), loop);
+    gbb_event_player_play_file(player, argv[1]);
+    g_main_loop_run (loop);
+
+    return 0;
+}
+
+static GOptionEntry play_options[] =
+{
+    { NULL }
+};
+
+static int
+play(int argc, char **argv)
+{
+    GbbEventPlayer *player = GBB_EVENT_PLAYER(gbb_remote_player_new("Gnome Battery Bench Test"));
+    return do_play(player, argc, argv);
+}
+
+static int
+play_local(int argc, char **argv)
+{
+    GbbEventPlayer *player = GBB_EVENT_PLAYER(gbb_evdev_player_new("Gnome Battery Bench Test"));
+    return do_play(player, argc, argv);
+}
+
+static char *record_output;
+
+static GOptionEntry record_options[] =
+{
+    { "output", 'o', 0, G_OPTION_ARG_FILENAME, &record_output, "Output file", "FILENAME" },
+    { NULL }
+};
+
+static int
+record(int argc, char **argv)
+{
+    GbbEventRecorder *recorder;
+
+    Display *display = XOpenDisplay(NULL);
+    if (!display)
+        die("Can't open X display %s", XDisplayName(NULL));
+
+    recorder = gbb_event_recorder_new(display, record_output);
+    gbb_event_recorder_record(recorder);
+    gbb_event_recorder_free(recorder);
+
+    return 0;
+}
+
+typedef struct {
+    const char *command;
+    const GOptionEntry *options;
+    int (*run) (int argc, char **argv);
+    int min_args;
+    int max_args;
+    const char *param_string;
+} Subcommand;
+
+Subcommand subcommands[] = {
+    { "monitor",      monitor_options, monitor, 0, 0 },
+    { "play",         play_options, play, 1, 1, "FILENAME" },
+    { "play-local",   play_options, play_local, 1, 1, "FILENAME" },
+    { "record",       record_options, record, 0, 0 },
+    { NULL }
+};
+
+static void global_usage(void) G_GNUC_NORETURN;
+
+static void
+global_usage(void)
+{
+    int i;
+    fprintf(stderr, "Usage: [");
+    for (i = 0; subcommands[i].command; i++) {
+        fprintf(stderr,
+                "%s%s", i == 0 ? "" : "|",
+                subcommands[i].command);
+    }
+    fprintf(stderr, "]\n");
+    exit(1);
 }
 
 int
 main(int argc, char **argv)
 {
+    int i;
+
     if (argc < 2)
-        usage();
+        global_usage();
 
-    if (strcmp(argv[1], "--record") == 0) {
-        if (argc != 2)
-            usage();
+    for (i = 0; subcommands[i].command; i++) {
+        Subcommand *subcommand = &subcommands[i];
 
-        record();
-    } else if (strcmp(argv[1], "--playback") == 0) {
-        if (argc != 3)
-            usage();
+        if (strcmp(subcommand->command, argv[1]) == 0) {
+            char *command = g_strconcat("gbb ", subcommand->command, NULL);
+            argv[1] = command;
+            argv += 1;
+            argc -= 1;
 
-        playback(argv[2]);
-    } else if (strcmp(argv[1], "--remote-playback") == 0) {
-        if (argc != 3)
-            usage();
+            GOptionContext *context = g_option_context_new(subcommand->param_string);
+            GError *error = NULL;
 
-        remote_playback(argv[2]);
-    } else if (strcmp(argv[1], "--test") == 0) {
-        if (argc != 2)
-            usage();
+            g_option_context_add_main_entries(context, subcommand->options, NULL);
+            if (!g_option_context_parse (context, &argc, &argv, &error))
+                die("option parsing failed: %s", error->message);
 
-        test();
-    } else {
-        usage();
+            if (argc < 1 + subcommand->min_args ||
+                argc > 1 + subcommand->max_args) {
+                char *help = g_option_context_get_help (context, TRUE, NULL);
+                fprintf(stderr, "%s", help);
+                exit(1);
+            }
+
+            return subcommand->run(argc, argv);
+        }
     }
 
-    return 0;
+    global_usage();
 }
